72d4ac83f2698b9d1f9d91bcd772f92ebfbff400
Chirag Baid <126846374+ChiragBaid@users.noreply.github.com>
2025-02-14 18:15:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier    
+from sklearn.ensemble import RandomForestClassifier   
 from sklearn.metrics import accuracy_score, classification_report
 import joblib
+import seaborn as sns
 
-data = pd.read_csv('2022\heart_2022_with_nans.csv')
+data = pd.read_csv('2020\heart_2022_no_nans.csv')
 
 print(data.head())
 
@@ -14,14 +15,14 @@ y = data.iloc[:, -1]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-model = DecisionTreeClassifier()
+model = RandomForestClassifier(random_state = 42)
 
 if x_train.isnull().values.any() or x_test.isnull().values.any():
     print("NaN values are still present in the dataset after imputation.")
     x_train = x_train.fillna(0)
     x_test = x_test.fillna(0)
 
-x_train = pd.get_dummies(x_train)
+x_train = pd.get_dummies(x_train)   
 x_test = pd.get_dummies(x_test)
 
 x_train, x_test = x_train.align(x_test, join='left', axis=1, fill_value=0)
